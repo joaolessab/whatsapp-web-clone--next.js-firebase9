9cab1256525db89d2b3a8d316c18d43ad559bfc4
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2023-01-05 23:32:57 -0300

    Sidebar Notification

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -3,6 +3,8 @@ import { Avatar, IconButton } from '@mui/material'
 import ChatIcon from '@mui/icons-material/Chat'
 import CustomMoreVertical from './CustomMoreVertical'
 import SearchIcon from '@mui/icons-material/Search'
+import NotificationsOffIcon from '@mui/icons-material/NotificationsOff'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 
 const Sidebar = () => { 
     return (
@@ -19,6 +21,20 @@ const Sidebar = () => {
                     <CustomMoreVertical />
                 </IconsGroup>
             </Header>
+            <Notification>
+                <NotificationAvatar>
+                    <NotificationsOffIcon style={{ color: '#9DE1FE' }} />
+                </NotificationAvatar>
+                <NotificationText>
+                    <div>Get Notification of New Messages</div>
+                    <div style={{ display: 'flex', alignItems: 'center'}}>
+                        <a href="#"><u>Turn on Desktop notifications</u></a>
+                        <IconButton>
+                            <ArrowForwardIosIcon style={{width: 15, height: 15}} />
+                        </IconButton>
+                    </div>
+                </NotificationText>
+            </Notification>
             <SearchChat>
                 <SearchBar>
                     <SearchIcon />
@@ -77,4 +93,21 @@ const SearchBar = styled.div`
 const SearchInput = styled.input`
     width: 100%;
     border: none;
+`
+
+const Notification = styled.div`
+    display: flex;
+    justify-content: space-around;
+    aling-items: center;
+    padding: 10px;
+    background-color: #9DE1FE;
+`
+
+const NotificationAvatar = styled(Avatar)`
+    background-color: white;
+`
+
+const NotificationText = styled.div`
+    display: flex;
+    flex-direction: column;
 `
