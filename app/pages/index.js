5987c311800a93a4d5c7dc3e028c80788078d1be
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2023-01-05 23:15:35 -0300

    Alt

--- app/pages/index.js
+++ app/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
       
       <div style={{width: '50%', textAlign: 'center'}}>
-        <Image src="/whatsapp-home.png" height={250} width={250} />
+        <Image src="/whatsapp-home.png" height={250} width={250} alt=''/>
         <h2 style={{ color: '#727372' }}>Keep your phone connected</h2>
         <p style={{ color: '#b7b9bb' }}>
           WhatsApp connects to your phone to sync messages. To reduce data usage, connect your phone to Wi-Fi.
