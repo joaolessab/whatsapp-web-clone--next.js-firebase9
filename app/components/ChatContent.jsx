300edb430076038a819b87fd269ba4cd6376176e
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2023-01-07 19:07:09 -0300

    Input for Sending Messages on ChatBox

--- app/components/ChatContent.jsx
+++ app/components/ChatContent.jsx
@@ -2,6 +2,9 @@ import { Avatar, IconButton } from '@mui/material'
 import styled from 'styled-components'
 import SearchIcon from '@mui/icons-material/Search'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon'
+import AttachFileIcon from '@mui/icons-material/AttachFile'
+import MicIcon from '@mui/icons-material/Mic'
 
 const ChatContent = () => { 
     return (
@@ -19,6 +22,18 @@ const ChatContent = () => {
                     <MoreVertIcon />
                 </IconButton>
             </Header>
+            <InputContainer>
+                <IconButton>
+                    <InsertEmoticonIcon />
+                </IconButton>
+                <IconButton>
+                    <AttachFileIcon />
+                </IconButton>
+                <Input placeholder='Type a message' />
+                <IconButton>
+                    <MicIcon />
+                </IconButton>
+            </InputContainer>
         </Container>
     )
     
@@ -55,4 +70,24 @@ const HeaderInfo = styled.div`
         font-size: 14px;
         color: gray;
     }
+`
+
+const InputContainer = styled.form`
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    position: sticky;
+    bottom: 0;
+    background-color: #f0f0f0;
+    z-index: 100;
+`
+
+const Input = styled.input`
+    flex: 1;
+    outline: 0;
+    border: none;
+    border-radius: 30px;
+    padding: 20px;
+    margin-left: 15px;
+    margin-right: 15px;
 `
