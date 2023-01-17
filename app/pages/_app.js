1a749f19a3c793b743f9dd7381b1c91582c9b4b7
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2023-01-17 10:07:39 -0300

    Head Titles

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,14 +1,23 @@
+import Head from 'next/head'
 import { AuthProvider } from '../Auth'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthProvider>
+    <>
+      <Head>
+        <title>WhatsApp Web - by @joaolessab</title>
+        <meta name="description" content="Created by @joaolessab" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </>
   )
 }
 
